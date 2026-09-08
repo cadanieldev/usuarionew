66f26c2ff1c9a244bd5bcbd040688b201ef79c13
Daniel <carlosdanielsantos458@gmail.com>
2026-09-07 23:49:37 -0300

    adicionado metodo que salva dados de usuario

---
+++ src/main/java/com/javanauta/usuario/business/converter/UsuarioConverter.java
@@ -0,0 +1,110 @@
+package com.javanauta.usuario.business.converter;
+
+import com.javanauta.usuario.business.dto.EnderecoDTO;
+import com.javanauta.usuario.business.dto.TelefoneDTO;
+import com.javanauta.usuario.business.dto.UsuarioDTO;
+import com.javanauta.usuario.infrastructure.entity.Endereco;
+import com.javanauta.usuario.infrastructure.entity.Telefone;
+import com.javanauta.usuario.infrastructure.entity.Usuario;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // componente , nao é uma service
+
+public class UsuarioConverter {
+
+
+    public Usuario paraUsuario(UsuarioDTO usuarioDTO){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome()) // dentro do atributo nome da entitdade usuario passando o usuarioDTO getnome..
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefones(usuarioDTO.getTelefones()))
+                .build();
+    }
+
+    public List<Endereco> paraListaEndereco(List<EnderecoDTO> enderecoDTOS){ // transformar em lista com java stream
+//        return enderecoDTOS.stream().map(this::paraEndereco).toList();
+        List<Endereco> enderecos = new ArrayList<>();
+        for(EnderecoDTO enderecoDTO : enderecoDTOS){
+            enderecos.add(paraEndereco(enderecoDTO));
+        }
+        return enderecos;
+
+    }
+
+    public Endereco paraEndereco(EnderecoDTO enderecoDTO){ //Conversao
+        return Endereco.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<Telefone> paraListaTelefones(List<TelefoneDTO> telefoneDTOS){
+        return telefoneDTOS.stream().map(this::paraTelefone).toList(); // transformando tudo em uma lista, mesma funçao do FOR
+    }
+
+    public Telefone paraTelefone(TelefoneDTO telefoneDTO){ // Conversão dto para entity
+        return Telefone.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+
+    public UsuarioDTO paraUsuarioDTO(Usuario usuarioDTO){
+        return UsuarioDTO.builder()
+                .nome(usuarioDTO.getNome()) // dentro do atributo nome da entitdade usuario passando o usuarioDTO getnome..
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEnderecoDTO(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefonesDTO(usuarioDTO.getTelefones()))
+                .build();
+    }
+
+    public List<EnderecoDTO> paraListaEnderecoDTO(List<Endereco> enderecoDTOS){ // transformar em lista com java stream
+//        return enderecoDTOS.stream().map(this::paraEndereco).toList();
+        List<EnderecoDTO> enderecos = new ArrayList<>();
+        for(Endereco enderecoDTO : enderecoDTOS){
+            enderecos.add(paraEnderecoDTO(enderecoDTO));
+        }
+        return enderecos;
+
+    }
+
+    public EnderecoDTO paraEnderecoDTO(Endereco enderecoDTO){ //Conversao
+        return EnderecoDTO.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<TelefoneDTO> paraListaTelefonesDTO(List<Telefone> telefoneDTOS){
+        return telefoneDTOS.stream().map(this::paraTelefoneDTO).toList(); // transformando tudo em uma lista, mesma funçao do FOR
+    }
+
+    public TelefoneDTO paraTelefoneDTO(Telefone telefoneDTO){ // Conversão dto para entity
+        return TelefoneDTO.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+
+
+
+
+
+}
